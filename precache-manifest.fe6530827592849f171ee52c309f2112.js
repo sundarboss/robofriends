82a7d786d39304ecaa4944e6ce5304c89cef43e7
Sundar <s.sundar_100@yahoo.com>
2019-12-02 18:33:17 +0530

    Updates

---
+++ precache-manifest.fe6530827592849f171ee52c309f2112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be97bd7c4f88916c735bb59041c4aeca",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47bd7c824e1ee43ce79d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "42c7c29e4ef65415782b",
+    "url": "/robofriends/static/css/main.dbed6ef4.chunk.css"
+  },
+  {
+    "revision": "47bd7c824e1ee43ce79d",
+    "url": "/robofriends/static/js/2.6b65a726.chunk.js"
+  },
+  {
+    "revision": "42c7c29e4ef65415782b",
+    "url": "/robofriends/static/js/main.c518fcad.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "9cd3334ed917794ef2e1da896e000309",
+    "url": "/robofriends/static/media/MingguPagi.9cd3334e.woff"
+  }
+]);
